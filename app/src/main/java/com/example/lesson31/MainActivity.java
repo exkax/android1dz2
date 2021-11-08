c7736587d7f3be90165eb3c9787744f9bbfb921d
exkax <exkaxbilal@gmail.com>
2021-11-08 14:39:46 +0600

    first push

--- app/src/main/java/com/example/lesson31/MainActivity.java
+++ app/src/main/java/com/example/lesson31/MainActivity.java
@@ -8,13 +8,16 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
     private Button go_buttom;
+    private ImageView image;
     private TextInputEditText Username,password;
 
     @Override
@@ -24,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
         go_buttom = findViewById(R.id.go);
         Username = findViewById(R.id.f1);
         password = findViewById(R.id.f2);
+        image = findViewById(R.id.image);
+        initListeners();
+
+
 
 
         Username.addTextChangedListener(new TextWatcher() {
@@ -49,5 +56,10 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+
+    private void initListeners() {
+        Glide.with(this).load("https://i.pinimg.com/474x/23/ab/a6/23aba60b66ef08174bb7455c4a8a2d2f.jpg").into(image);
     }
 }
